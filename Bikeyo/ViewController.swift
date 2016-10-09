713f75b47d0d415ed3077b733713099d3a6ae011
Leteyski <ruslan.leteyski@save.co>
2016-10-10 00:09:12 +0200

    Implemented User Location

--- Bikeyo/ViewController.swift
+++ Bikeyo/ViewController.swift
@@ -11,38 +11,35 @@ import MapKit
 import CoreLocation
 import Alamofire
 
+//48.869809, 2.395010
 //JCD api key a8fe986f0dc47defeccdb202251be114363e20c1
 // Stations request: https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=a8fe986f0dc47defeccdb202251be114363e20c1
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var Map: MKMapView!
+    let locationManager = CLLocationManager()
+    var location = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       // var location = CLLocationCoordinate2D(latitude: 48.870333, longitude: 2.346769)
         
-        let location = CLLocationCoordinate2D(latitude: 48.870333, longitude: 2.346769)
-        let span = MKCoordinateSpanMake(0.01, 0.01)
-        let region = MKCoordinateRegion(center: location, span: span)
-
-        Map.setRegion(region, animated: true)
-        
-        
-        Alamofire.request("https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=a8fe986f0dc47defeccdb202251be114363e20c1").responseJSON { response in
-            print(response.request)  // original URL request
-            print(response.response) // HTTP URL response
-            print(response.data)     // server data
-            print(response.result)   // result of response serialization
-            
-            if let JSON = response.result.value as? [Dictionary<String,Any>] {
-                print("JSONBEGINS ---- \(JSON)")
-                self.setupStations(data: JSON)
-            } else {
-                print("Request failed somewhere")
-            }
+  //      let stationData: [Dictionary<String,Any>] = []
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
         }
         
         
+        
+        setupMap()
+        
+        
+       getStations(url: "https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=a8fe986f0dc47defeccdb202251be114363e20c1")
 
     }
 
@@ -51,6 +48,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func getStations(url: String) {
+        Alamofire.request(url).responseJSON { response in
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value as? [Dictionary<String,Any>] {
+                print("JSONBEGINS ---- \(JSON)")
+                self.setupStations(data: JSON)
+        
+            } else {
+                print("Request failed somewhere")
+            }
+        }
+    }
     
     
     func setupStations(data: [Dictionary<String,Any>]) {
@@ -95,6 +109,27 @@ class ViewController: UIViewController {
                 }
             }
         }
+    
+    func setupMap() {
+        
+        let span = MKCoordinateSpanMake(0.01, 0.01)
+        let region = MKCoordinateRegion(center: location, span: span)
+        Map.setRegion(region, animated: true)
+        Map.showsUserLocation = true
+        
+        
+
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        
+        self.location.latitude = locValue.latitude
+        self.location.longitude = locValue.longitude
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        locationManager.stopUpdatingLocation()
+        setupMap()
+    }
         
 }
 
