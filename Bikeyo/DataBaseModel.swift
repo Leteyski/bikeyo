859b8d358d82f710f418ae7d34dbf08d8fccdb26
Leteyski <ruslan.leteyski@save.co>
2016-10-22 23:41:11 +0200

    v1 Final Fixes Implemented

---
+++ Bikeyo/DataBaseModel.swift
@@ -0,0 +1,16 @@
+//
+//  StationModel.swift
+//  Bikeyo
+//
+//  Created by Ruslan Leteyski on 10/10/16.
+//  Copyright © 2016 Leteyski. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+
+class JSONObject: Object {
+    dynamic var data = NSData()
+}
+
--- Bikeyo/ViewController.swift
+++ Bikeyo/ViewController.swift
@@ -12,19 +12,9 @@ import CoreLocation
 import Alamofire
 import RealmSwift
 
-//48.869809, 2.395010
 //JCD api key a8fe986f0dc47defeccdb202251be114363e20c1
 // Stations request: https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=a8fe986f0dc47defeccdb202251be114363e20c1
 
-
-
-/*
- var detailItem: AnyObject? {
- didSet {
- // Update the view.
- self.configureView()
- }
- }*/
 
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
    
@@ -42,15 +32,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     var location = CLLocationCoordinate2D(latitude: 48.855324, longitude: 2.345074)
     let realm = try! Realm()
     let requestURL = "https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=a8fe986f0dc47defeccdb202251be114363e20c1"
+    var globalJSON: [Dictionary<String,Any>] = []
     
-    // Pins
-    
-  //  var pointAnnotation: StationPointAnnotation!
-    var pinAnnotationView: MKPinAnnotationView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        setupSegmentedControl()
         
         setupMap()
         getLocation()
@@ -65,7 +54,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     }
     
     
-    // Data Functions
+    // MARK: Request Methods
     
     func updateStations(APIRequestURL url: String) {
         
@@ -80,11 +69,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             
             
             if let JSON = response.result.value as? [Dictionary<String,Any>] {
-                print("JSONBEGINS ---- \(JSON)")
+                
+                // print("JSONBEGINS ---- \(JSON)")
                 
                 self.setupStations(data: JSON)
                 
                 //Caching
+                
+                self.globalJSON = JSON
                 
                 let jsonObject = JSONObject()
                 jsonObject.data = response.data! as NSData
@@ -98,12 +90,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             } else {
                 print("Request failed somewhere")
                 self.showAlert(title: "Request failed!", message: "The app couldn't fetch current data, please try again later.")
+                
                 let jsonObject = self.realm.objects(JSONObject.self)
                 
                 if jsonObject.count > 0 {
                     for json in jsonObject {
                     let finalData = try! JSONSerialization.jsonObject(with: json.data as Data, options: []) as? [Dictionary<String,Any>]
                     self.setupStations(data: finalData!)
+                    self.globalJSON = finalData!
                     }
                 } else {
                     self.ActivityIndicator.isHidden = true
@@ -113,44 +107,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         }
     }
     
-    // View Functions
+    func getLocation() {
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+    }
     
+    // MARK: View Setup Methods
     
     func setupStations(data: [Dictionary<String,Any>]) {
         
         self.Map.removeAnnotations(self.Map.annotations)
-        
-       /* for station in data {
-            
-            if let position = station["position"] as? [String:AnyObject] {
-                if let latitude = position["lat"] as? Double,
-                    let longitude = position["lng"] as? Double {
-                    
-                    let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-                    let annotation = MKPointAnnotation()
-                    
-                    annotation.coordinate = location
-                    
-                    if let name = station["name"] as? String {
-                        annotation.title = name
-                    } else {
-                        annotation.title = "Name unavailable"
-                    }
-                    
-                    if let availableBikes = station["available_bikes"] as? Int,
-                        let availableBikeStands = station["available_bike_stands"] as? Int {
-                        annotation.subtitle = "Available Bikes: \(availableBikes) ; Available Stands: \(availableBikeStands)"
-                    } else {
-                        annotation.subtitle = "Data unvailable"
-                    }
-                    
-                    self.Map.addAnnotation(annotation)
-                    
-                }
-            
-            }
-            
-        } */
         
         for station in data {
             
@@ -167,9 +139,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
                     guard let isBonus = station["bonus"] as? Bool else { return }
                     
                     let stationPoint = StationPoint(title: name, coordinate: location, subtitle: address, availableBikes: availableBikes, availableStands: availableStands, isBonus: isBonus)
-                    //stationPoint.pinLabel.text = "\(stationPoint.availableBikes)"
+                    
                     stationPoint.getPinLabel(segmentedControlSegmentIndex: self.segmentedControl.selectedSegmentIndex)
                     stationPoint.getPinImage(segmentedControlSegmentIndex: self.segmentedControl.selectedSegmentIndex)
+                    
                     self.Map.addAnnotation(stationPoint)
                 
                 }
@@ -183,27 +156,66 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
     }
     
-    func getLocation() {
-       
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.requestWhenInUseAuthorization()
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-            locationManager.startUpdatingLocation()
-        }
-
-    }
     
     func setupMap() {
         
         let span = MKCoordinateSpanMake(0.01, 0.01)
         let region = MKCoordinateRegion(center: location, span: span)
         Map.setRegion(region, animated: true)
-      //  Map.showsUserLocation = true
         Map.delegate = self
-        //Map.mapType = MKMapType.standard
 
     }
+    
+    func setupSegmentedControl() {
+        
+        segmentedControl.setDividerImage(UIImage.init(named: "segmentSeparator"), forLeftSegmentState: .disabled, rightSegmentState: .normal, barMetrics: .default)
+        segmentedControl.layer.borderColor = UIColor(colorLiteralRed: 234/255.0, green: 234/255.0, blue: 234/255.0, alpha: 1.0).cgColor
+        segmentedControl.layer.borderWidth = CGFloat(1)
+        
+        segmentedControl.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.init(red: 103/255.0, green: 35/255.0, blue: 176/255.0, alpha: 1.0), NSFontAttributeName: UIFont.boldSystemFont(ofSize: 14)], for: .selected)
+        segmentedControl.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.init(red: 160/255.0, green: 160/255.0, blue: 160/255.0, alpha: 1.0), NSFontAttributeName: UIFont.boldSystemFont(ofSize: 14)],  for: .normal)
+    }
+    
+    func updateSegmentedControl(indicator: UIView) {
+        
+        if segmentedControl.selectedSegmentIndex == 1 {
+            
+            segmentedControl.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.init(red: 103/255.0, green: 35/255.0, blue: 176/255.0, alpha: 1.0), NSFontAttributeName: UIFont.boldSystemFont(ofSize: 12)], for: .selected)
+            segmentedControl.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.init(red: 160/255.0, green: 160/255.0, blue: 160/255.0, alpha: 1.0), NSFontAttributeName: UIFont.boldSystemFont(ofSize: 12)],  for: .normal)
+            
+            indicator.backgroundColor = UIColor(colorLiteralRed: 103/255.0, green: 35/255.0, blue: 176/255.0, alpha: 1.0)
+            indicator.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                indicator.bottomAnchor.constraint(equalTo: segmentedControl.bottomAnchor),
+                indicator.rightAnchor.constraint(equalTo: segmentedControl.rightAnchor)
+                ])
+        } else {
+            
+            segmentedControl.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.init(red: 255/255.0, green: 145/255.0, blue: 5/255.0, alpha: 1.0), NSFontAttributeName: UIFont.boldSystemFont(ofSize: 12)], for: .selected)
+            segmentedControl.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.init(red: 160/255.0, green: 160/255.0, blue: 160/255.0, alpha: 1.0), NSFontAttributeName: UIFont.boldSystemFont(ofSize: 12)],  for: .normal)
+            
+            indicator.backgroundColor = UIColor(colorLiteralRed: 255/255.0, green: 145/255.0, blue: 5/255.0, alpha: 1.0)
+            indicator.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                indicator.bottomAnchor.constraint(equalTo: segmentedControl.bottomAnchor),
+                indicator.leftAnchor.constraint(equalTo: segmentedControl.leftAnchor)
+                ])
+
+        }
+    }
+    
+    func showAlert(title: String, message: String?) {
+        let alertControler = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertControler.addAction(alertAction)
+        present(alertControler, animated: true, completion: nil)
+    }
+    
+    
+    //MARK: Delegate Methods
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let locValue:CLLocationCoordinate2D = manager.location!.coordinate
@@ -216,54 +228,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         setupMap()
     }
     
-  /* func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
-        //if mapView.reuseIdentifier
-        
-        if annotation is StationPoint {
-            let reuseIdentifier = "stationPin"
-            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
-            let stationPoint = annotation as! StationPoint
-            //let numberLabel = UILabel()
-            //numberLabel.text = String(stationPoint.availableBikes)
-            
-            if annotationView == nil {
-                annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseIdentifier)
-                annotationView?.canShowCallout = true
-                
-                annotationView?.addSubview(stationPoint.numberLabel)
-                
-                stationPoint.numberLabel.textColor = UIColor.white
-                stationPoint.numberLabel.font = UIFont.boldSystemFont(ofSize: 13)
-                //numberLabel.text = "\(stationPoint.availableBikes)"
-                
-                stationPoint.numberLabel.translatesAutoresizingMaskIntoConstraints = false
-                NSLayoutConstraint.activate([
-                    stationPoint.numberLabel.topAnchor.constraint(equalTo: (annotationView?.topAnchor)!, constant: 12),
-                    stationPoint.numberLabel.centerXAnchor.constraint(equalTo: (annotationView?.centerXAnchor)!)
-                    ])
-
-            } else {
-                annotationView?.annotation = annotation
-                //numberLabel.text = "\(stationPoint.availableBikes)"
-            }
-            
-            //let stationPointAnnotation = annotation as! StationPointAnnotation
-            if stationPoint.isBonus == true {
-                annotationView?.image = UIImage(named: "yellowPinPlus")
-            } else {
-                annotationView?.image = UIImage(named: "yellowPin")
-            }
-            
-            return annotationView
-        }
-        
-        return nil
-    } */
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
-        //if mapView.reuseIdentifier
         
         if annotation is StationPoint {
             
@@ -284,13 +250,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
                     stationPoint.pinLabel.centerXAnchor.constraint(equalTo: (annotationView.centerXAnchor))
                     ])
             
-            //let stationPointAnnotation = annotation as! StationPointAnnotation
-           /* if stationPoint.isBonus == true {
-                annotationView.image = UIImage(named: "yellowPinPlus")
-            } else {
-                annotationView.image = UIImage(named: "yellowPin")
-            } */
-            
+         
             annotationView.image = stationPoint.pinImage!
             return annotationView
         }
@@ -299,20 +259,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     }
     
     
-    
-    
-    
-    func showAlert(title: String, message: String?) {
-        let alertControler = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        
-        alertControler.addAction(alertAction)
-        present(alertControler, animated: true, completion: nil)
-    }
-    
-    
-    
-    // IB Actions
+    // MARK: IB Actions
     
     @IBAction func RefreshStations() {
         updateStations(APIRequestURL: requestURL)
@@ -323,16 +270,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     }
     
     @IBAction func SegmentedControlIndexChanged(_ sender: AnyObject) {
-        
-        let jsonObject = self.realm.objects(JSONObject.self)
-        
-        for json in jsonObject {
-                let finalData = try! JSONSerialization.jsonObject(with: json.data as Data, options: []) as? [Dictionary<String,Any>]
-                self.setupStations(data: finalData!)
-        }
-        
-        print("Segmented Control reloads stations")
     
+        setupStations(data: globalJSON)
+        
     }
 }
 
